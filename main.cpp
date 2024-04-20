66bcce14c5646d1dcfbaab7db456e014aa212570
Salupol <y.pavel.t@gmail.com>
2024-04-20 18:36:39 +0300

    2

--- main.cpp
+++ main.cpp
@@ -60,12 +60,53 @@ public:
         }
         cout << endl;
     }
+
 };
 
 int main() {
     MySet set;
     while (1) {
-        printf("press any key to start \n");
+        printf("hello, choose action: \n 1. insert digits in set \n 2. remove digits from set \n 3. print set \n 4. check if digit in set \n 5. exit \n");
+        int choice;
+        cin >> choice;
+        switch(choice){
+            case 1:
+                char answer1;
+                do{
+                double answer;
+                printf("Enter the number to be inserted: ");
+                cin >> answer;
+                set.insert(answer);
+                printf("number \"%.2f\" is inserted \n", answer);
+                printf("Do you want to insert more numbers? (y/n): ");
+                cin >> answer1;
+                } while (answer1 != 'n');
+                break;
+            case 2:
+                double answer2;
+                printf("Enter the number to be removed: ");
+                cin >> answer2;
+                set.remove(answer2);
+                break;
+            case 3:
+                set.printset();
+                break;
+            case 4:
+                double answer3;
+                printf("Enter the number to be checked: ");
+                cin >> answer3;
+                if(set.contains(answer3)){
+                    printf("digit \"%.2f\" is in the set \n", answer3);
+                } else {
+                    printf("digit \"%.2f\" is not in the set \n", answer3);
+                }
+
+            case 5:
+                return 0;
+            default:
+                cout << "Invalid choice" << endl;
+        }
+        /*printf("press any key to start \n");
         cin.ignore();
         cin.ignore(10000, '\n');
         double answer;
@@ -76,7 +117,7 @@ int main() {
         } else {
             set.insert(answer);
         }
-        set.printset();
+        set.printset();*/
     }
     printf("the size of set is %d", set.size);
     return 0;
